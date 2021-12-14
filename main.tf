74e1ff5f2c1dafe7b04a657712e71d6b57f0d522
Olivier Hoareau <oha@greenberets.io>
2021-12-14 08:45:52 +0100

    chore: up modules

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 module "website" {
   source         = "genstackio/website/aws"
-  version        = "0.1.0"
+  version        = "0.1.53"
   name           = var.name
   bucket_name    = var.bucket_name
   zone           = var.dns_zone
